a8a9310497bd5104c3fde02c488b94492447f66d
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-08-12 15:32:17 -0500

    Empaquetado final Logos

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,8 +24,9 @@
 
         figure.mb-5
           .video.mb-2
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/3WgVcRSDEa8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/csBIqH2bBsE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
           figcaption Video: herramientas y normativas de innovación en salud pública.
+
 
 </template>
 
